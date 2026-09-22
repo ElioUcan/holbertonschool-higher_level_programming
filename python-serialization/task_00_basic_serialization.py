c1315f6effa15ea2692eebf77fee0236751341c8
ElioUcan <eliozapata2006@gmail.com>
2026-09-21 19:16:36 -0600

    Finished

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
+"""
+Adds the functionality to serialize
+a Python dictionary to a JSON file and deserialize
+the JSON file to recreate a Python dictionary
 
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize and saves a file"""
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """deserialize a file"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
